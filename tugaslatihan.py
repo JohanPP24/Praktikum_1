287909ed5544cd1ff32e16028d59ec676ef69198
JohanPP24 <92928548+JohanPP24@users.noreply.github.com>
2021-10-26 22:26:29 +0700

    Create tugaslatihan.py

---
+++ tugaslatihan.py
@@ -0,0 +1,14 @@
+a=input("Nama lengkap:")
+b=input("Nama panggilan:")
+c=input("NPM:")
+d=input("tempat lahir:")
+e=input("umur:")
+f=input("alamat:")
+g=input("telepon:")
+  
+print("Assalamulaikum")
+
+print("Let me introduce my self. My name is",a,"but you can call me",b)
+print("My NPM is",c,".I was born in ",d,"and I am ",e,"years old.")
+print("I am very glad if you want to ivite my house in",f,".")
+print("So, don't forget to call me before with the number",g)
